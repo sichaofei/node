a9e3ff460d12e75e87c68e2dad67a604a42d7dcd
Your Name <you@example.com>
2019-06-29 21:25:30 +0800

    23

--- server/upload.js
+++ server/upload.js
@@ -1,7 +1,22 @@
 var sendFn=require("./send")
-module.exports=function(app,uploadImg){
+module.exports=function(app,multer){
+	var url=""
+	const storage = multer.diskStorage({
+	  // destination:'public/uploads/'+new Date().getFullYear() + (new Date().getMonth()+1) + new Date().getDate(),
+	  // 设置存储文件地址
+	  destination(req,res,cb){
+	    cb(null,"public/images");
+	  },
+	  // 设置存储文件名字
+	  filename(req,file,cb){
+	    const filenameArr = file.originalname.split('.');
+	    url=Date.now() + '.' + filenameArr[filenameArr.length-1]
+	    console.log(url)
+	    cb(null,url);
+	  }
+	});
+	const uploadImg = multer({storage}).single('multfile');
     app.post("/upload/headImg",uploadImg,function(req,res){
-        console.log(req.body)
-        sendFn(res,1,"",{})
+    	sendFn(res,1,"",{})
     })
 }
